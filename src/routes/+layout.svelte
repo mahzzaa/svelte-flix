5a52588f4556e31ae1bf09553835e4c5e3f3d42d
mah <mahsa.79mirmiran@gmail.com>
2023-12-09 01:02:36 +0300

    20th part 1 finished

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script>
   import "../style.css";
+  import logo from "$lib/images/logo.png";
+  import tmdb from "$lib/images/tmdb.svg";
 </script>
 
 <nav>
-  <a href="/"> SvelteFlix </a>
+  <img src={logo} alt="logo" class="logo" />
 
   <div class="links">
     <a href="/search"> Search</a>
@@ -11,6 +13,18 @@
     <a href="/login"> Log in</a>
   </div>
 </nav>
+
+<main>
+  <slot />
+</main>
+
+<footer>
+  <p>Data provided by</p>
+
+  <a href="https://www.themoviedb.org/">
+    <img src={tmdb} alt="tmsb" class="tmdb" /></a
+  >
+</footer>
 
 <style>
   nav {
@@ -27,5 +41,24 @@
   a {
     color: inherit;
     text-decoration: none;
+  }
+
+  .links {
+    display: flex;
+    gap: 1rem;
+  }
+
+  .logo {
+    width: 10%;
+  }
+
+  .tmdb {
+    width: 30%;
+  }
+
+  footer {
+    display: flex;
+    justify-content: center;
+    height: 5rem;
   }
 </style>
