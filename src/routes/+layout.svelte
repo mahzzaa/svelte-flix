b8e4a4fb1471fb0a989106888b2840de394f4f71
mah <mahsa.79mirmiran@gmail.com>
2023-12-08 23:38:18 +0300

    minute 8:16

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+  import '../style.css';
+</script>
+
+<nav>
+  <a href="/">
+    SvelteFlix
+  </a>
+
+  <div class="links">
+    <a href="/search"> Search</a>
+    <a href="/watchlist"> Watchlist</a>
+    <a href="/login"> Log in</a>
+
+  </div>
+</nav>
+
